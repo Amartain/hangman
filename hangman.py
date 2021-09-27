ae3e54e4b4f061c22a8b8ab88f9af8add8ccfca1
valaki55 <61266640+valaki55@users.noreply.github.com>
2021-09-27 15:11:09 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 import random
 
-#helyes/helytelen tipp lista létrehozása
+#változók
+
 correctg=[] #helyes tippek
 incorrectg=[] #helytelen tippek
 acsik=[]
@@ -20,40 +21,45 @@ for elem in acsik:
     print(elem, end=" ")
 print("\n")
 
+#találgatás
+while True:
+    if "_" in acsik:
+        #felhasználótól bekért betű > csere az acsikban
+        uin=input("Guess a letter: ")
+        #a játékos beírja a megoldást
+        if uin==solution:
+            print("Congrats! You have found the solution!")
+            break
+        if len(uin)>1 and uin!=solution:
+            print("Wrong tipp!")
+        #hosszáadja a megfelelő listához
+        if uin in solution:
+            correctg.append(uin)
+        else:
+            incorrectg.append(uin)
+        print("Correct guesses:")
+        for elem in correctg:
+            print(elem, end="\t")
+        print("\nIncorrect guesses:")
+        for elem in incorrectg:
+            print(elem, end="\t")
 
-#felhasználótól bekért betű > csere az acsikban
-uin=input("Guess a letter: ")
-
-
-#hosszáadja a megfelelő listához
-
-if uin in solution:
-    correctg.append(uin)
-else:
-    incorrectg.append(uin)
-for elem in correctg:
-    print(elem, end="\t")
-print("\n")
-for elem in incorrectg:
-    print(elem, end="\t")
-
-#a felhasználó tippje hanyadik betű a megoldásban
-corpl=[] #a betű indexeinek tárolása
-for i in range(len(solution)):
-    if solution[i]==uin:
-        corpl.append(i)
-print(corpl)
-
-acsik=[]
-
-for i in range(len(solution)):
-    if i in corpl:
-        acsik.append(uin)
-        #print(uin, end=" ")
+        #a felhasználó tippje hanyadik betű a megoldásban
+        corpl=[] #a betű indexeinek tárolása
+        for i in range(len(solution)):
+            if solution[i]==uin:
+                corpl.append(i)
+        #print(corpl)
+        #az acsikban kicseréli az uinra a corpl-ban lévő indexű elemeket
+        for i in range(len(solution)):
+            if i in corpl:
+                acsik[i]=uin
+        print("\n")
+        #print(acsik)
+        for elem in acsik:
+            print(elem, end=" ")
+        print("")
     else:
-        acsik.append("_")
-        #print("_", end=" ")
-#print(acsik)
-for elem in acsik:
-    print(elem, end=" ")
+        print("Congrats! You have found the solution!")
+        break
 
